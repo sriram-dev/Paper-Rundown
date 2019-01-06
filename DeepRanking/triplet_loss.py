2df7fbc324a848acbed4be2d56da1b28a8d04c43
Sriram Desikan <sriram.desikan@xome.com>
2019-01-06 15:01:17 +0530

    deep ranking

---
+++ DeepRanking/triplet_loss.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import numpy as np
+import os
+import matplotlib.image as mpimg
+from numpy import genfromtxt
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+import h5py
+import matplotlib.pyplot as plt
+import pylab as pl
+import numpy as np
+from PIL import Image
+from keras import backend as k 
+def triplet_loss(y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+    
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+    
+    Returns:
+    loss -- real number, value of the loss
+    """
+    
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+    ### START CODE HERE ### (≈ 4 lines)
+    # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), -1)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), -1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss,  0))
+    ### END CODE HERE ###
+    
+    return k.mean(loss)
+	
+def img_to_encoding(image_path, model, width, height):
+    img = Image.open(image_path)
+    img = img.resize((width,height))
+    x_train= np.asarray(img)
+    x_train = np.array([x_train])
+    embedding = model.predict_on_batch(x_train)
+    return embedding
+def identity_loss(y_true, y_pred):
+    return y_pred
